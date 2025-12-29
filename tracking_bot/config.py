c07225808d2eedc6981996fcf2053b6dc92240d6
SergFomi <35821775+SergFomi@users.noreply.github.com>
2025-12-29 15:11:25 +0700

    Update config.py

--- tracking_bot/config.py
+++ tracking_bot/config.py
@@ -20,6 +20,7 @@ SCHEDULE = [
     {"time": "12:00", "question": "Завтрак?", "type": "yes_no"},
     {"time": "15:00", "question": "Обед?", "type": "yes_no"},
     {"time": "18:00", "question": "Ужин?", "type": "yes_no"},
+    {"time": "18:00", "question": "Встречал человека с классификацией DISC?", "type": "text"},
     {"time": "19:00", "question": "Спорт?", "type": "yes_no"},
     {"time": "21:00", "question": "Что делал сегодня?", "type": "text"},
     {"time": "21:00", "question": "Продуктивность?", "type": "scale", "range": (1, 10)},
